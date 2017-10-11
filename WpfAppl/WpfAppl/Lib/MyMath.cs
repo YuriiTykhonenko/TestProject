c540d92b94421131468d50755d9920469d44d264
Yurii Tykhonenko <yurii.tykhonenko@asposeptyltd.com>
2017-10-11 12:04:55 +0300

    Added project

---
+++ WpfAppl/WpfAppl/Lib/MyMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppl.Lib
+{
+    public class MyMath
+    {
+        public int AddInt(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
